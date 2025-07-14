7e69ad7eedf28cda50577272f5db557ce306045a
sasso0101 <git@sasso.dev>
2025-07-14 02:54:58 +0200

    add id back

--- src/merged_csr.h
+++ src/merged_csr.h
@@ -15,6 +15,7 @@ typedef struct {
 #define DATA_SIZE 1
 #define DEGREE(mer, i) mer->merged[i]
 #define DISTANCE(mer, i) mer->merged[i + 1]
+#define ID(mer, i) mer->merged[i+2]
 
 /**
  * Converts the CSR graph into a modified merged CSR format with embedded
